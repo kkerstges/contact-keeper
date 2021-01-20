02b568c57240114c50c3447532577792a56a033d
Klaus <klaus.kerstges@gmail.com>
2021-01-20 09:49:28 +0100

    Filter contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -5,13 +5,21 @@ import ContactItem from './ContactItem';
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    <h4>Please add a contact</h4>;
+  }
 
   return (
     <Fragment>
-      {contacts.map((contact) => (
-        <ContactItem contact={contact} key={contact.id}></ContactItem>
-      ))}
+      {filtered !== null
+        ? filtered.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))
+        : contacts.map((contact) => (
+            <ContactItem contact={contact} key={contact.id}></ContactItem>
+          ))}
     </Fragment>
   );
 };
