1db755a0ca92f8c9b0d834a27a3ec9636cacb4d1
Klaus <klaus.kerstges@gmail.com>
2021-01-20 18:10:59 +0100

    Add user to db

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -9,7 +9,7 @@ const Contacts = () => {
   const { contacts, filtered } = contactContext;
 
   if (contacts.length === 0) {
-    <h4>Please add a contact</h4>;
+    return <h4>Please add a contacts...</h4>;
   }
 
   return (
@@ -17,12 +17,12 @@ const Contacts = () => {
       <TransitionGroup>
         {filtered !== null
           ? filtered.map((contact) => (
-              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+              <CSSTransition key={contact._id} timeout={500} classNames='item'>
                 <ContactItem contact={contact} />
               </CSSTransition>
             ))
           : contacts.map((contact) => (
-              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+              <CSSTransition key={contact._id} timeout={500} classNames='item'>
                 <ContactItem contact={contact}></ContactItem>
               </CSSTransition>
             ))}
