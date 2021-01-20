f0f5fede3881d5d9614ec4caf54b2fcf2eb98c8c
Klaus <klaus.kerstges@gmail.com>
2021-01-20 16:26:19 +0100

    Load User & Set Token

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,19 +2,24 @@ import React, { useContext, useState, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
 import AuthContext from '../../context/auth/authContext';
 
-const Register = () => {
+const Register = (props) => {
   const alertContext = useContext(AlertContext);
   const authContext = useContext(AuthContext);
 
   const { setAlert } = alertContext;
-  const { register, clearErrors, error } = authContext;
+  const { register, clearErrors, error, isAuthenticated } = authContext;
 
   useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/');
+    }
+
     if (error === 'User already exists') {
       setAlert(error, 'danger');
       clearErrors();
     }
-  }, [clearErrors, error, setAlert]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [error, isAuthenticated, props.history]);
 
   const [user, setUser] = useState({
     name: '',
--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -12,7 +12,15 @@ import {
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
   switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload,
+      };
     case REGISTER_SUCCESS:
+    case AUTH_ERROR:
       localStorage.setItem('token', action.payload.token);
       return {
         ...state,
